07a93a26a0b2575ad9fab47700243d804c2c6f57
HughesSeanKyle <khughessean@yahoo.com>
2023-04-29 22:19:30 +0200

    main data types

--- 2.1-variables-data-types.c
+++ 2.1-variables-data-types.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
+    // Numbers
     int a = 10;
     /*
         %lu - unsigned long
@@ -9,12 +11,30 @@ int main()
         // Output for "b" below is 8 bytes (1 byte === 8 bits) (8*8=64)
         // Output for "c" below is 4 bytes (1 byte === 8 bits) (4*8=32)
 
+        Use float when really constrained in terms of memory size
+        - Note that c can run on different devices
+            - If you are building for a device that has small memory this
+                - should be taken into account
     */
     double b = 10.5; // 64 bit number (More bits == more precision)
     printf("%lu\n", sizeof(b));
 
     float c = 10.5; // 32 bit number
     printf("%lu\n", sizeof(c));
+
+    // Strings
+    /*
+        char will just store one char
+    */
+
+    char d = 'a';
+    char e[/*Specify length of string here (null termi included)*/] = "char array";
+
+    // Booleans
+    /*
+        To use need to #include <stdbool.h>
+    */
+    bool f = false; // or true
 
     return 0;
 }
