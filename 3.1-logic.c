f810cf4a5e6fa3ea73dd2f9aa6aed6b8e999eda1
HughesSeanKyle <khughessean@yahoo.com>
2023-04-30 13:24:27 +0200

    Simple if statement

---
+++ 3.1-logic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/*
+    Includes
+        - if statements and variations
+        - Switch statements
+        - ternary operator
+*/
+int main()
+{
+    if (true)
+    {
+        printf("this is true\n");
+    }
+    return 0;
+}
