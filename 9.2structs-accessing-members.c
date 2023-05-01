91cc28d90cfb0df9d50c0795271ba1f488b9301b
HughesSeanKyle <khughessean@yahoo.com>
2023-05-01 20:40:49 +0200

    create second instance of struct

--- 9.2structs-accessing-members.c
+++ 9.2structs-accessing-members.c
@@ -11,14 +11,24 @@ int main()
 {
     // Create a structure variable of myStructure called s1
     struct myStructure s1;
+    // Additional structu from main struct
+    struct myStructure s2;
 
     // Assign values to members of s1
     s1.myNum = 13;
     s1.myLetter = 'B';
 
+    // Assign values to members of s2
+    s2.myNum = 9;
+    s2.myLetter = 'q';
+
     // Print values
     printf("My number: %d\n", s1.myNum);
     printf("My letter: %c\n", s1.myLetter);
+
+    // Print values of s2
+    printf("My number: %d\n", s2.myNum);
+    printf("My letter: %c\n", s2.myLetter);
 
     return 0;
 }
