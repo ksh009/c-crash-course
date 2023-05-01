68acab02c49c3b8e6ac89a9c06e3c3d79c996ae6
HughesSeanKyle <khughessean@yahoo.com>
2023-05-01 19:20:10 +0200

    create file in specific dir

--- 8.1-c-files.c
+++ 8.1-c-files.c
@@ -4,10 +4,19 @@ int main()
 {
     FILE *fptr;
 
+    /*
+        Create a file in the same directory
+    */
     // Create a file on your computer (filename.txt)
-    fptr = fopen("filename.txt", "w");
+    // fptr = fopen("filename.txt", "w");
 
     // Close the file
+    // fclose(fptr);
+
+    /*
+        Create a file in a specific directory
+    */
+    fptr = fopen("/Users/user_name/Desktop/example.txt", "w");
     fclose(fptr);
 
     return 0;
